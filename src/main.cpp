6f62a474e2aea43438e82969c5379b2508dfa2f3
SnailDragon <tylernielsenn@gmail.com>
2024-12-14 13:50:28 -0700

    sd card version?

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,15 @@
  * default SD Library
  */
 
-#include "SD.h"
 #include "Adafruit_TinyUSB.h"
+#include "SD.h"
+#include "SdFat.h"
 
-const int chipSelect = 10;
+void msc_flush_cb(void);
+int32_t msc_write_cb(uint32_t lba, uint8_t* buffer, uint32_t bufsize);
+int32_t msc_read_cb(uint32_t lba, void* buffer, uint32_t bufsize);
+
+const int chipSelect = 17;
 
 Adafruit_USBD_MSC usb_msc;
 
@@ -24,11 +29,12 @@ Sd2Card card;
 SdVolume volume;
 
 // the setup function runs once when you press reset or power the board
-void setup()
-{
+void setup() {
   Serial.begin(115200);
+  while(!Serial); 
 
-  // Set disk vendor id, product id and revision with string up to 8, 16, 4 characters respectively
+  // Set disk vendor id, product id and revision with string up to 8, 16, 4
+  // characters respectively
   usb_msc.setID("Adafruit", "SD Card", "1.0");
 
   // Set read write callback
@@ -39,35 +45,40 @@ void setup()
   usb_msc.setUnitReady(false);
   usb_msc.begin();
 
-  // If already enumerated, additional class driverr begin() e.g msc, hid, midi won't take effect until re-enumeration
+  // If already enumerated, additional class driverr begin() e.g msc, hid, midi
+  // won't take effect until re-enumeration
   if (TinyUSBDevice.mounted()) {
     TinyUSBDevice.detach();
     delay(10);
     TinyUSBDevice.attach();
   }
 
-  //while ( !Serial ) delay(10);   // wait for native usb
+  // while ( !Serial ) delay(10);   // wait for native usb
   Serial.println("Adafruit TinyUSB Mass Storage SD Card example");
   Serial.println("\nInitializing SD card...");
 
-  if ( !card.init(SPI_HALF_SPEED, chipSelect) ) {
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
     Serial.println("initialization failed. Things to check:");
     Serial.println("* is a card inserted?");
     Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    Serial.println(
+        "* did you change the chipSelect pin to match your shield or module?");
     while (1) delay(1);
   }
 
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or
+  // FAT32
   if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    Serial.println(
+        "Could not find FAT16/FAT32 partition.\nMake sure you've formatted the "
+        "card");
     while (1) delay(1);
   }
-  
-  uint32_t block_count = volume.blocksPerCluster()*volume.clusterCount();
+
+  uint32_t block_count = volume.blocksPerCluster() * volume.clusterCount();
 
   Serial.print("Volume size (MB):  ");
-  Serial.println((block_count/2) / 1024);
+  Serial.println((block_count / 2) / 1024);
 
   // Set disk size, SD block size is always 512
   usb_msc.setCapacity(block_count, 512);
@@ -83,21 +94,21 @@ void loop() {
 // Callback invoked when received READ10 command.
 // Copy disk's data to buffer (up to bufsize) and
 // return number of copied bytes (must be multiple of block size)
-int32_t msc_read_cb (uint32_t lba, void* buffer, uint32_t bufsize) {
-  (void) bufsize;
-  return card.readBlock(lba, (uint8_t*) buffer) ? 512 : -1;
+int32_t msc_read_cb(uint32_t lba, void* buffer, uint32_t bufsize) {
+  (void)bufsize;
+  return card.readBlock(lba, (uint8_t*)buffer) ? 512 : -1;
 }
 
 // Callback invoked when received WRITE10 command.
-// Process data in buffer to disk's storage and 
+// Process data in buffer to disk's storage and
 // return number of written bytes (must be multiple of block size)
-int32_t msc_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize) {
-  (void) bufsize;
+int32_t msc_write_cb(uint32_t lba, uint8_t* buffer, uint32_t bufsize) {
+  (void)bufsize;
   return card.writeBlock(lba, buffer) ? 512 : -1;
 }
 
-// Callback invoked when WRITE10 command is completed (status received and accepted by host).
-// used to flush any pending cache.
-void msc_flush_cb (void) {
+// Callback invoked when WRITE10 command is completed (status received and
+// accepted by host). used to flush any pending cache.
+void msc_flush_cb(void) {
   // nothing to do
 }
