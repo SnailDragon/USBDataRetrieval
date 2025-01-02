5955a3e1ca48927bad30e351e1ec29a2cbce6503
SnailDragon <tylernielsenn@gmail.com>
2025-01-02 11:13:39 -0500

    standardized format

---
+++ scripts/standardizeFormat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Standardizes the format of every .h, .cpp, and .ino file in the repo
+# Intended to be executed in the OrganizationalScripts folder 
+
+cd ..
+
+FILES=$(find -not -path "*/.*" -not -path "*/lib/*" -not -path "*/docs/*" | grep -E "\.c|\.h|\.cpp|\.ino$")
+
+echo "Formatting:"
+echo $FILES
+
+clang-format -style=Google -i $FILES
+
+echo "Done."
